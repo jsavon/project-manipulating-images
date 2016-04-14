d2acc94a44baf0750ee8ab246ce04b5a9a4c15b3
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-04-14 03:03:32 -0400

    v2
    v2

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -9,32 +9,32 @@ logoWidth, logoHeight = logoIm.size
 
 os.makedirs('withLogo', exist_ok=True)
 os.chdir('./originals')
-
+#TODO: Loop over all files in the working directory
 for filename in os.listdir('./originals'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
     or filename == LOGO_FILENAME:
         continue
-im = Image.open(filename)
+
+    im = Image.open('./originals/' + filename)
     width, height = im.size
 
+#TODO: Check if file image needs to be resized
+#TODO: Calculate the new width and height to resize to.
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-    if width > height
-       height = int((SQUARE_FIT_SIZE / width) * height)
-       width = SQUARE_FIT_SIZE
-    else:
-       width = int((SQUARE_FIT_SIZE / height) * width)
-       height = SQUARE_FIT_SIZE
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
 
+    im = im.resize((width, height))        
     print('Resizing %s...' % (filename))
-    im = im.resize((width, height))
+        
 
+#TODO: Add the logo.
     print('Adding logo to %s...' % (filename))
     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-    im.save(os.path.join('../withLogo', filename))
-
-#TODO: Loop over all files in the working directory
-#TODO: Check if file image needs to be
-#TODO: Calculate the new width and height to resize to.
-#TODO: Add the logo.
 #TODO: Save changes.
+    im.save(os.path.join('withLogo', filename))
