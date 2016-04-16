e85db08799114617d834ef93754a4202203ea3ae
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-04-16 16:24:09 -0400

    ..

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -11,21 +11,21 @@ os.makedirs('withLogo', exist_ok=True)
 for filename in os.listdir('./originals/'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
     or filename == LOGO_FILENAME:
-        continue
+       continue
 
     im = Image.open('./originals/' + filename)
     width, height = im.size
 
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-        if width > height:
+       if width > height:
             height = int((SQUARE_FIT_SIZE / width) * height)
             width = SQUARE_FIT_SIZE
-        else:
+       else:
             width = int((SQUARE_FIT_SIZE / height) * width)
             height = SQUARE_FIT_SIZE
 
-    im = im.resize((width, height))        
     print('Resizing %s...' % (filename))
+    im = im.resize((width, height))
     
     print('Adding logo to %s...' % (filename))
     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
